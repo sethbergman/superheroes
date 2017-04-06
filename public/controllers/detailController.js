b11fe7ba14bb4ebc787c8e06db330c19742d64d6
Seth Bergman <seth.atxwebs@gmail.com>
2017-04-06 07:00:01 -0500

    Fixed detailController

--- public/controllers/detailController.js
+++ public/controllers/detailController.js
@@ -1,14 +1,12 @@
 var detailCtrl = angular.module('detailCtrl', []);
 detailCtrl.controller('detailController', function($scope, $http, $routeParams){
-	// $scope.superhero = {};
-	var store = this;
-	store.superhero = [ ];
+	$scope.superhero = {};
 	//get the id to query the db and retrieve the correct superhero
 	var id = $routeParams.id;
 	$http.get('/superhero/' + id)
 		.success(function(data){
 			console.log(JSON.stringify(data));
-			.superhero = data;
+			$scope.superhero = data;
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
